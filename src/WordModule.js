30b0a5f0a8380cddf3065f08f011e02b76ddc57e
Brian Archibald <brianallenarchibald@gmail.com>
2020-05-11 21:15:50 -0500

    added module css

--- src/WordModule.js
+++ src/WordModule.js
@@ -7,10 +7,10 @@ function WordModule() {
   // function to determine the color of background of modules, clicked or not and the danger one.
      return (
       <div className="grid">
-        <div className="word-module"><Word /></div>
-        <div className="word-module"><Word /></div>
-        <div className="word-module"><Word /></div>
-        <div className="word-module"><Word /></div>
+        <div className="word-module red revealed"><Word /></div>
+        <div className="word-module blue revealed"><Word /></div>
+        <div className="word-module nuetral revealed"><Word /></div>
+        <div className="word-module black-hiddenword"><Word /></div>
         <div className="word-module"><Word /></div>
         <div className="word-module"><Word /></div>
         <div className="word-module"><Word /></div>
